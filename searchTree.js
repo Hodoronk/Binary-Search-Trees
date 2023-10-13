dbde724bf515290ccad3fb839e6e7a741c48b0cd
Hodoronk <tanasa.andrei@hotmail.com>
2023-10-13 19:18:50 +0300

    added function that removes duplicates

--- searchTree.js
+++ searchTree.js
@@ -14,6 +14,31 @@ class Tree {
 
 const treeArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
-const buildTree = (array) => {
+const buildTree = (array, n = 0) => {
 
+    
+
+
+
+
+    if(n == array.length - 1) {
+
+    } else {
+
+        if(array[n+1] > array[n+2]) {
+            array[n].right = array[n+1]
+            array[n].left = array[n+2]
+        } else {
+            array[n].right = array[n+2]
+            array[n].left = array[n+1]
+        }
+    }
 }
+
+const removeDuplicates = (array) => {
+    return array.filter((value, index, self) => {
+        return self.indexOf(value) === index;
+    })
+}
+console.log(treeArray) ;
+console.log(removeDuplicates(treeArray)) ;

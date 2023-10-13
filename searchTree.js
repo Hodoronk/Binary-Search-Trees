80edaa8c1d6ef24b7cf994668002c852845f3f8d
Hodoronk <tanasa.andrei@hotmail.com>
2023-10-13 19:06:02 +0300

    added Node class, Tree class and buildTree function

---
+++ searchTree.js
@@ -0,0 +1,19 @@
+class Node {
+    constructor (data = null, left = null, right = null) {
+        this.data = data
+        this.left = left
+        this.right = right
+    }
+}
+
+class Tree {
+    constructor(root){
+        this.root = root
+    }
+}
+
+const treeArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+
+const buildTree = (array) => {
+
+}

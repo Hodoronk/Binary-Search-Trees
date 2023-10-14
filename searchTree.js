c2a8f0369ce7f8ee075128eacef14eed9f30ba1b
Hodoronk <tanasa.andrei@hotmail.com>
2023-10-14 15:58:43 +0300

    refactor buildTree, added sortArray function

--- searchTree.js
+++ searchTree.js
@@ -1,8 +1,8 @@
 class Node {
-    constructor (data = null, left = null, right = null) {
-        this.data = data
-        this.left = left
-        this.right = right
+    constructor (value) {
+        this.value = value
+        this.left = null
+        this.right = null
     }
 }
 
@@ -12,33 +12,37 @@ class Tree {
     }
 }
 
-const treeArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-
-const buildTree = (array, n = 0) => {
-
-    
-
-
-
-
-    if(n == array.length - 1) {
-
-    } else {
-
-        if(array[n+1] > array[n+2]) {
-            array[n].right = array[n+1]
-            array[n].left = array[n+2]
-        } else {
-            array[n].right = array[n+2]
-            array[n].left = array[n+1]
-        }
-    }
-}
-
 const removeDuplicates = (array) => {
     return array.filter((value, index, self) => {
         return self.indexOf(value) === index;
     })
 }
-console.log(treeArray) ;
-console.log(removeDuplicates(treeArray)) ;
+
+
+const treeArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+const sortArray = (array) => {
+    const noDuplicates = removeDuplicates(array);
+    return noDuplicates.sort((a, b) => a - b)
+}
+const newArray = sortArray(treeArray)
+console.log(newArray) 
+
+
+const buildTree = (array) => {
+    array = sortArray(array) ;
+    if(array.length === 0) {
+        return null
+    } else {
+        const middleNode = Math.floor(array.length / 2) ;
+        const root = new Node(array[middleNode])
+
+        root.right = buildTree(array.slice(0, middleNode))
+        root.left = buildTree(array.slice(middleNode + 1))
+
+        return root;
+    }
+}
+
+console.log(buildTree(treeArray)) ;
+
+

010ea67279cf1e190796cddddbb2b26d7780d11e
Hodoronk <tanasa.andrei@hotmail.com>
2023-10-14 17:24:12 +0300

    fix insert function

--- searchTree.js
+++ searchTree.js
@@ -1,3 +1,12 @@
+const removeDuplicates = (array) => {
+    return array.filter((value, index, self) => {
+        return self.indexOf(value) === index;
+    })
+}
+const sortArray = (array) => {
+    const noDuplicates = removeDuplicates(array);
+    return noDuplicates.sort((a, b) => a - b)
+}
 class Node {
     constructor (value) {
         this.value = value
@@ -7,42 +16,60 @@ class Node {
 }
 
 class Tree {
-    constructor(root){
-        this.root = root
+    constructor(array){
+        this.root = this.buildTree(array)
+    }
+
+    buildTree (array) {
+        array = sortArray(array) ;
+        if(array.length === 0) {
+            return null
+        } else {
+            const middleNode = Math.floor(array.length / 2) ;
+            const root = new Node(array[middleNode])
+    
+            root.right = this.buildTree(array.slice(0, middleNode))
+            root.left = this.buildTree(array.slice(middleNode + 1))
+    
+            return root;
+        }
+    }
+    insert(value, root) {
+        if(root === null) {
+            return new Node(value);
+        }if(value === root.value) {
+            console.log("Value already exists inside Binary Search Tree")
+            return root
+        }
+        if(value > root.value){
+            root.right = this.insert(value, root.right)
+        }else{
+            root.right = this.insert(value, root.left)
+        }
+        return root
     }
 }
 
-const removeDuplicates = (array) => {
-    return array.filter((value, index, self) => {
-        return self.indexOf(value) === index;
-    })
-}
+
+const myArray = [42, 17, 68, 23, 55, 8, 37, 91, 13, 62];
+const myTree = new Tree(myArray);
+myTree.buildTree(myArray);
+myTree.insert(23, myTree.root)
 
 
-const treeArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-const sortArray = (array) => {
-    const noDuplicates = removeDuplicates(array);
-    return noDuplicates.sort((a, b) => a - b)
-}
-const newArray = sortArray(treeArray)
-console.log(newArray) 
+// const prettyPrint = (node, prefix = "", isLeft = true) => { prettyPrint function from the Odin Project
+//     if (node === null) {
+//       return;
+//     }
+//     if (node.right !== null) {
+//       prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+//     }
+//     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+//     if (node.left !== null) {
+//       prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+//     }
+//   };
 
-
-const buildTree = (array) => {
-    array = sortArray(array) ;
-    if(array.length === 0) {
-        return null
-    } else {
-        const middleNode = Math.floor(array.length / 2) ;
-        const root = new Node(array[middleNode])
-
-        root.right = buildTree(array.slice(0, middleNode))
-        root.left = buildTree(array.slice(middleNode + 1))
-
-        return root;
-    }
-}
-
-console.log(buildTree(treeArray)) ;
+  prettyPrint(myTree.root)
 
 
